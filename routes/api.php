369e5603cf0dc2ec0c7a69beacae8500153f6763
Nikola Vujcic <nikola.vujcic.001@gmail.com>
2022-12-29 23:05:39 +0100

    Laravel-domaci

--- routes/api.php
+++ routes/api.php
@@ -24,30 +24,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-//Route::get('slopes/{id}', [SlopeController::class, 'show']);
-//Route::get('slopes', [SlopeController::class, 'index']);
-
-//Route::resource('slopes', SlopeController::class);
-
-Route::get('/mountains/{id}/slopes', [MountainSlopeController::class, 'index'])->name('mountains.slopes.index');
-
-Route::get('users', [UserController::class, 'index']);
-
-Route::get('users/{id}', [UserController::class, 'show']);
-
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/profile', function (Request $request) {
-        return auth()->user();
-    }
-    );
-    Route::resource('slopes', SlopeController::class)->only(['update', 'store', 'destroy']);
-
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-Route::resource('slopes', SlopeController::class)->only(['index']);
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('users', [UserController::class, 'index']);
-Route::get('users/{id}', [UserController::class, 'show']);
-
 
